bfaba8dae6b26934d1e942761fc317277090d4fd
Psychou <Green97@gmx.net>
2017-03-04 21:28:35 +0100

    Maybe now Tablet Menu

--- Pentagaming.Tanoa/dialog/settings.hpp
+++ Pentagaming.Tanoa/dialog/settings.hpp
@@ -5,6 +5,17 @@ class SettingsMenu {
     enableSimulation = 1;
 
     class controlsBackground {
+		
+		class InvTablet: Life_RscPicture
+		{
+			idc = 9090909;
+			text = "textures\menu.paa";
+			x = 0;
+			y = -0.12;
+			w = 1;
+			h = 1.28;
+		};
+		
         class RscTitleBackground: Life_RscText {
             colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
             idc = -1;
