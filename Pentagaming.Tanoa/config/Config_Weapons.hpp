d0501e10dd887a16ae3f8a0beec607c8ccd58f18
Psychou <Green97@gmx.net>
2017-03-06 00:21:55 +0100

    S.W.A.T. Shop und Cop Shop Anpassungen... Glaub ich :dash:

--- Pentagaming.Tanoa/config/Config_Weapons.hpp
+++ Pentagaming.Tanoa/config/Config_Weapons.hpp
@@ -145,14 +145,50 @@ class WeaponShops {
 
     //Cop Shops
     class cop_basic {
-        name = "Altis Cop Shop";
+        name = "Tanoa Cop Shop";
         side = "cop";
         conditions = "";
         items[] = {
             { "Binocular", "", 150, 75, "" },
             { "ItemGPS", "", 100, 50, "" },
             { "FirstAidKit", "", 150, 75, "" },
-            { "NVGoggles", "", 2000, 1000, "" },
+            { "NVGoggles_OPFOR", "", 2000, 1000, "" },
+            { "hgun_P07_snds_F", $STR_W_items_StunPistol, 2000, 1000, "" },
+            { "hgun_P07_F", "", 7500, 3750, "" },
+            { "hgun_P07_khk_F", "", 7500, 3750, "" }, //Apex DLC
+            { "hgun_Pistol_heavy_01_F", "", 9500, 4750, "call life_coplevel >= 1" },
+            { "SMG_02_ACO_F", "", 30000, 15000, "call life_coplevel >= 2" },
+            { "arifle_MX_F", "", 35000, 17500, "call life_coplevel >= 2" },
+            { "hgun_ACPC2_F", "", 17500, 8750, "call life_coplevel >= 3" },
+            { "arifle_MXC_F", "", 30000, 15000, "call life_coplevel >= 3" }
+        };
+        mags[] = {
+            { "16Rnd_9x21_Mag", "", 125, 60, "" },
+            { "20Rnd_556x45_UW_mag", $STR_W_mags_TaserRifle, 125, 60, "" },
+            { "11Rnd_45ACP_Mag", "", 130, 65, "call life_coplevel >= 1" },
+            { "30Rnd_65x39_caseless_mag", "", 130, 65, "call life_coplevel >= 2" },
+            { "30Rnd_9x21_Mag", "", 250, 125, "call life_coplevel >= 2" },
+            { "9Rnd_45ACP_Mag", "", 200, 100, "call life_coplevel >= 3" }
+        };
+        accs[] = {
+            { "muzzle_snds_L", "", 650, 325, "" },
+            { "optic_MRD", "", 2750, 1375, "call life_coplevel >= 1" },
+            { "acc_flashlight", "", 750, 375, "call life_coplevel >= 2" },
+            { "optic_Holosight", "", 1200, 600, "call life_coplevel >= 2" },
+            { "optic_Arco", "", 2500, 1250, "call life_coplevel >= 2" },
+            { "muzzle_snds_H", "", 2750, 1375, "call life_coplevel >= 2" }
+        };
+    };
+
+	class cop_swat {
+        name = "Tanoa S.W.A.T Shop";
+        side = "cop";
+        conditions = "license_cop_swat";
+        items[] = {
+            { "Binocular", "", 150, 75, "" },
+            { "ItemGPS", "", 100, 50, "" },
+            { "FirstAidKit", "", 150, 75, "" },
+            { "NVGoggles_OPFOR", "", 2000, 1000, "" },
             { "HandGrenade_Stone", $STR_W_items_Flashbang, 1700, 850, "" },
             { "hgun_P07_snds_F", $STR_W_items_StunPistol, 2000, 1000, "" },
             { "arifle_SDAR_F", $STR_W_items_TaserRifle, 20000, 10000, "" },
@@ -183,7 +219,7 @@ class WeaponShops {
             { "muzzle_snds_H", "", 2750, 1375, "call life_coplevel >= 2" }
         };
     };
-
+	
     //Medic Shops
     class med_basic {
         name = "store";
